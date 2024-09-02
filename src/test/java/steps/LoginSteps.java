b714954e764e736556c59068fc93dd0ecf488c39
Elenika <1991pugacheva@gmail.com>
2024-09-02 13:28:36 -0400

    Final

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,17 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import pages.SignInPage;
+import utility.SeleniumUtility;
+
+public class SignInStep extends SeleniumUtility {
+    //Sign in page steps
+   /* @Then("user enter {string} in the username field")
+    public void enterUsernameOnSignInPage(String value){
+        sendText(SignInPage.USERNAME,value);
+    }
+    @Then("user enter {string} in the password field")
+    public void enterPasswordOnSignInPage(String value){
+        sendText(SignInPage.PASSWORD,value);
+        }*/
+}
